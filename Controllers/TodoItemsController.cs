546e742dbe056f5270abb0d1269678ffc36aa228
Khaled Alrusan <khaled66658032@gmail.com>
2023-05-30 10:50:02 -0400

    Closes #1

--- Controllers/TodoItemsController.cs
+++ Controllers/TodoItemsController.cs
@@ -44,7 +44,6 @@ namespace TodoAPI.Controllers
         }
 
         // PUT: api/TodoItems/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
         public async Task<IActionResult> PutTodoItem(long id, TodoItem todoItem)
         {
